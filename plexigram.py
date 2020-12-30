6f9278d43582b992dfe0ea8035d05f30aadabe6e
cavebutter <jay@realpm.net>
2020-12-30 10:17:07 -0800

    ready to add telegram

--- plexigram.py
+++ plexigram.py
@@ -2,6 +2,7 @@ from plexapi.myplex import MyPlexAccount
 from plexapi.server import PlexServer
 import plexconfig as p
 from datetime import datetime, date, time, timedelta
+import telegram
 
 #############################
 #    Plex Authentication    #
@@ -9,6 +10,11 @@ from datetime import datetime, date, time, timedelta
 
 account = MyPlexAccount(p.username, p.password)
 plex = account.resource(p.servername).connect()
+
+###################################
+#    Plex functions that return   #
+#    lists of media.              #
+###################################
 
 #  List all shows of specified genre
 def list_shows(genre):
@@ -45,3 +51,22 @@ def new_tv():
         shows_list.append(summary_item)
     new_seasons = set(shows_list)
     return new_seasons
+
+#  List of Recently Added Masterclasses
+def new_masterclass():
+    shows_list = []
+    new_tv = plex.library.section('Masterclass').recentlyAdded()
+    shows = [show for show in new_tv]
+    for show in shows:
+        summary_item = show.grandparentTitle + ' - ' + show.parentTitle
+        shows_list.append(summary_item)
+    new_seasons = set(shows_list)
+    return new_seasons
+
+#  This one is not working as intended.  Returns a list of Artist objects and I can't
+#  get album info from that.
+def new_music():
+    #music_list = []
+    new_music = plex.library.section('Music').recentlyAdded()
+    music_list = [music for music in new_music]
+    return music_list
